87665c8bcb8619d134e8254e9c76a5ef8973bbb8
ku00 <skentaro36@gmail.com>
2017-05-13 23:46:01 +0900

    remake!

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,52 @@
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
+module Main where
 
-import Options.Generic
+import Options.Applicative
+import Data.Semigroup ((<>))
 
-data SearchWord = SearchWord String deriving (Generic, Show)
+data NewCmd = NewCmd { newProject :: String } deriving (Show)
 
-instance ParseRecord SearchWord
+-- data AddCmd = AddCmd { targetProject :: String, task :: String } deriving (Show)
+--
+-- data RunCmd = RunCmd { runProject :: String } deriving (Show)
 
-main = do
-    x <- getRecord "Find and display GIFs"
-    print (x :: SearchWord)
+data Commands = Commands
+    {
+      newCmd :: NewCmd
+    -- , addCmd :: AddCmd
+    -- , runCmd :: RunCmd
+    } deriving (Show)
+
+commands :: Parser Commands
+commands = Commands
+    <$> subparser (command "new" new)
+    -- <*> subparser (command "add" add)
+    -- <*> subparser (command "run" run)
+
+newOpt :: Parser NewCmd
+newOpt = NewCmd
+    <$> projectNameOpt
+    where
+      projectNameOpt :: Parser String
+      projectNameOpt = strOption
+          ( short 'p'
+         <> long "PROJECT_NAME"
+         <> help "A project name."
+         <> metavar "[PROJECT_NAME]" )
+
+new :: ParserInfo NewCmd
+new = info (helper <*> newOpt)
+    ( fullDesc
+   <> progDesc "new desc"
+   <> header "new header" )
+
+-- add :: ParserInfo AddCmd
+--
+-- run :: ParserInfo RunCmd
+
+parseInfo :: ParserInfo Commands
+parseInfo = info (helper <*> commands)
+    ( fullDesc
+   <> progDesc "main desc"
+   <> header "main header" )
+
+main = print =<< execParser parseInfo
