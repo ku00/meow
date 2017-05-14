d284debedf5edcd5cadf99a21d553cbd775ca025
ku00 <skentaro36@gmail.com>
2017-05-14 17:44:07 +0900

    Fixed `new` sub command

--- app/Main.hs
+++ app/Main.hs
@@ -3,71 +3,23 @@ module Main where
 import Options.Applicative
 import Data.Semigroup ((<>))
 
-data NewCmd = NewCmd { newProject :: String } deriving (Show)
+type Project = String
 
-data AddCmd = AddCmd { targetProject :: String, task :: String } deriving (Show)
+data Command = New Project deriving (Show)
 
--- data RunCmd = RunCmd { runProject :: String } deriving (Show)
+withInfo :: Parser a -> String -> ParserInfo a
+withInfo opts desc = info (helper <*> opts) $ progDesc desc
 
-data Commands = Commands
-    {
-      newCmd :: NewCmd
-    , addCmd :: AddCmd
-    -- , runCmd :: RunCmd
-    } deriving (Show)
+parseNew :: Parser Command
+parseNew = New
+    <$> argument str (metavar "PROJECT_NAME")
 
-commands :: Parser Commands
-commands = Commands
-    <$> subparser (command "new" new)
-    <*> subparser (command "add" add)
-    -- <*> subparser (command "run" run)
+parseCommand :: Parser Command
+parseCommand = subparser $
+    command "new" (parseNew `withInfo` "Create a project.")
 
-newOpt :: Parser NewCmd
-newOpt = NewCmd
-    <$> projectNameOpt
-    where
-        projectNameOpt :: Parser String
-        projectNameOpt = strOption
-            ( short 'p'
-           <> long "PROJECT_NAME"
-           <> help "A project name."
-           <> metavar "[PROJECT_NAME]" )
+parseInfo :: ParserInfo Command
+parseInfo = parseCommand `withInfo` "Meow is a command group management tool."
 
-new :: ParserInfo NewCmd
-new = info (helper <*> newOpt)
-    ( fullDesc
-   <> progDesc "new desc"
-   <> header "new header" )
-
-addOpt :: Parser AddCmd
-addOpt = AddCmd
-    <$> projectNameOpt
-    <*> taskStr
-    where
-        projectNameOpt :: Parser String
-        projectNameOpt = strOption
-            ( short 'p'
-           <> long "TARGET"
-           <> help "A target project" )
-
-        taskStr :: Parser String
-        taskStr = strOption
-            ( short 't'
-           <> long "task"
-           <> help "Add a task line." )
-
-add :: ParserInfo AddCmd
-add = info (helper <*> addOpt)
-    ( fullDesc
-   <> progDesc "add desc"
-   <> header "add header" )
-
--- run :: ParserInfo RunCmd
-
-parseInfo :: ParserInfo Commands
-parseInfo = info (helper <*> commands)
-    ( fullDesc
-   <> progDesc "main desc"
-   <> header "main header" )
-
+main :: IO ()
 main = print =<< execParser parseInfo
