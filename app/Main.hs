184a1d7f1eb84aea73c20dbe57694cf165f95f81
ku00 <skentaro36@gmail.com>
2017-05-14 12:19:37 +0900

    adds AddCmd
    Not working...

--- app/Main.hs
+++ app/Main.hs
@@ -5,33 +5,33 @@ import Data.Semigroup ((<>))
 
 data NewCmd = NewCmd { newProject :: String } deriving (Show)
 
--- data AddCmd = AddCmd { targetProject :: String, task :: String } deriving (Show)
---
+data AddCmd = AddCmd { targetProject :: String, task :: String } deriving (Show)
+
 -- data RunCmd = RunCmd { runProject :: String } deriving (Show)
 
 data Commands = Commands
     {
       newCmd :: NewCmd
-    -- , addCmd :: AddCmd
+    , addCmd :: AddCmd
     -- , runCmd :: RunCmd
     } deriving (Show)
 
 commands :: Parser Commands
 commands = Commands
     <$> subparser (command "new" new)
-    -- <*> subparser (command "add" add)
+    <*> subparser (command "add" add)
     -- <*> subparser (command "run" run)
 
 newOpt :: Parser NewCmd
 newOpt = NewCmd
     <$> projectNameOpt
     where
-      projectNameOpt :: Parser String
-      projectNameOpt = strOption
-          ( short 'p'
-         <> long "PROJECT_NAME"
-         <> help "A project name."
-         <> metavar "[PROJECT_NAME]" )
+        projectNameOpt :: Parser String
+        projectNameOpt = strOption
+            ( short 'p'
+           <> long "PROJECT_NAME"
+           <> help "A project name."
+           <> metavar "[PROJECT_NAME]" )
 
 new :: ParserInfo NewCmd
 new = info (helper <*> newOpt)
@@ -39,8 +39,29 @@ new = info (helper <*> newOpt)
    <> progDesc "new desc"
    <> header "new header" )
 
--- add :: ParserInfo AddCmd
---
+addOpt :: Parser AddCmd
+addOpt = AddCmd
+    <$> projectNameOpt
+    <*> taskStr
+    where
+        projectNameOpt :: Parser String
+        projectNameOpt = strOption
+            ( short 'p'
+           <> long "TARGET"
+           <> help "A target project" )
+
+        taskStr :: Parser String
+        taskStr = strOption
+            ( short 't'
+           <> long "task"
+           <> help "Add a task line." )
+
+add :: ParserInfo AddCmd
+add = info (helper <*> addOpt)
+    ( fullDesc
+   <> progDesc "add desc"
+   <> header "add header" )
+
 -- run :: ParserInfo RunCmd
 
 parseInfo :: ParserInfo Commands
