693fec3020096d3f2e2bc57e279b8e64b551b263
ku00 <skentaro36@gmail.com>
2017-05-14 19:10:49 +0900

    Add `Add` sub command

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,32 @@ module Main where
 import Options.Applicative
 import Data.Semigroup ((<>))
 
-type Project = String
+type Task = String
+type Line = String
 
-data Command = New Project deriving (Show)
+data Command
+    = New Task
+    | Add Task Line
+    deriving (Show)
 
 withInfo :: Parser a -> String -> ParserInfo a
 withInfo opts desc = info (helper <*> opts) $ progDesc desc
 
 parseNew :: Parser Command
-parseNew = New
-    <$> argument str (metavar "PROJECT_NAME")
+parseNew = New <$> argument str (metavar "[TASK_NAME]")
+
+parseAdd :: Parser Command
+parseAdd = Add
+    <$> argument str (metavar "[TARGET_TASK_NAME]")
+    <*> argument str (metavar "[TASK_LINE]")
 
 parseCommand :: Parser Command
 parseCommand = subparser $
-    command "new" (parseNew `withInfo` "Create a project.")
+    command "new" (parseNew `withInfo` "Create a task") <>
+    command "add" (parseAdd `withInfo` "Add a task line")
 
 parseInfo :: ParserInfo Command
-parseInfo = parseCommand `withInfo` "Meow is a command group management tool."
+parseInfo = parseCommand `withInfo` "Meow is a command group management tool"
 
 main :: IO ()
 main = print =<< execParser parseInfo
